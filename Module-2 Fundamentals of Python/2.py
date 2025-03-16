0ed0f86da8799ccca48ff642652bea46cc52d4e9
bhumijain26-4 <jainbhumi1331@gmail.com>
2025-03-16 22:57:58 +0530

    Delete Module-2 Fundamentals of Python/2.py

--- Module-2 Fundamentals of Python/2.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a Python program to get the Factorial number of given number. 
-"""
-fact = 1
-num = int(input("Enter any number: "))
-for i in range(1,num+1):
-  fact = fact*i
-print("The Factorial of given number is : ",end="")
-print(fact)
