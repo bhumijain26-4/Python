66b5159cfd857755c0db056a0bfc523e1e1b3439
bhumijain26-4 <jainbhumi1331@gmail.com>
2025-03-16 23:01:19 +0530

    Delete Module-2 Fundamentals of Python/8.py

--- Module-2 Fundamentals of Python/8.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a Python program to test whether a passed letter is a vowel or not.
-"""
-char = input("Enter any character: ")
-if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U' :
-    print("Vowel",end="")
-else:
-    print("Consonant",end="")
