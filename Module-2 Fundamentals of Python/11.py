3c29e4b150b2bdc933418b844c127a8fc578b20d
bhumijain26-4 <jainbhumi1331@gmail.com>
2025-03-16 22:57:05 +0530

    Delete Module-2 Fundamentals of Python/11.py

--- Module-2 Fundamentals of Python/11.py
+++
@@ -1,7 +0,0 @@
-"""
-Write a python program to sum of the first n positive integers.
-"""
-n = int(input("Enter any number: "))
-    
-sum = (n*(n+1))/2
-print(f"Sum of the first {n} Positive integers: {sum}")
