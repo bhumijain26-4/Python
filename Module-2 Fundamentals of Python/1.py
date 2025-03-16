4cfccc4c5d91f2a63ac23f1a933b579ac3e6ded3
bhumijain26-4 <jainbhumi1331@gmail.com>
2025-03-16 22:56:36 +0530

    Delete Module-2 Fundamentals of Python/1.py

--- Module-2 Fundamentals of Python/1.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a Python program that prints "Hello, World!".
-Set up Python on your local machine and write a program to display your name.
-"""
-
-print("Hello, World!")
-
-print("Bhumi")
